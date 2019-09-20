0846edf108181755cc9d799a044541150f3365aa
luis hernandez <nodnol4781@outlook.com>
2019-09-19 21:07:44 -0500

    Revicion hasta la entrada de variable

--- operacion.m
+++ operacion.m
@@ -1,13 +1,14 @@
 function operacion()
+    clc;
     Z=input('Z = '); %[-300 -400]; %Se define A como funcion objetivo
     a=input('coeficientes = '); %[ 3 3 ;3 6];     %Se define B como restricciones 
     b=input('b = '); %[ 120; 180];       %Se define C como recursos
-    sizea = size(a,1)
-    B = eye(sizea)
-    A=[a eye(sizea)]
-    ln = size(A,2)
-    C = [Z zeros(1,ln - size(Z,2))]
-    BCol=[size(a,2)+1:ln]
+    sizea = size(a,1);
+    B = eye(sizea);
+    A=[a eye(sizea)];
+    ln = size(A,2);
+    C = [Z zeros(1,ln - size(Z,2))];
+    BCol=[size(a,2)+1:ln];
     
     
     CBt = C(BCol(:));
@@ -16,16 +17,20 @@ function operacion()
     for i=1:ln
         Zi_Ci(i) = CBtB1 * A(:,i) - C(i);
     end
+    Zi_Ci
     %busca los negativos(borrar cometario)
     [Min_in Ind_in] = min(Zi_Ci);
     %--------------------------------
     fprintf('entra X%d\n',Ind_in);
-    B1b = B1 * b;
-    B1ai = B1 * A(:,Ind_in);
-    divX = B1b/B1ai;
-    divX = divX.';
-    divX = divX(1,:);
-    [Min_out Ind_out] = min(divX);
+            %sin conflictos%
+    
+    B1b = B1 * b
+    B1ai = B1 * A(:,Ind_in)
+    divX = B1b/B1ai
+    divX = divX.'
+    divX = divX(1,:)
+    [Min_out Ind_out] = min(divX)
+    
     fprintf('sale X%d\n',BCol(Ind_out));
     BCol(Ind_out)= Ind_in;
     B(:,Ind_out) = A(:,Ind_in)
