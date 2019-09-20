9b5c34551697d1b87cd28db3dde3d7d5d07c23e3
luis hernandez <nodnol4781@outlook.com>
2019-09-19 20:21:37 -0500

    Correccion de Bcol

--- operacion.m
+++ operacion.m
@@ -2,12 +2,12 @@ function operacion()
     Z=input('Z = '); %[-300 -400]; %Se define A como funcion objetivo
     a=input('coeficientes = '); %[ 3 3 ;3 6];     %Se define B como restricciones 
     b=input('b = '); %[ 120; 180];       %Se define C como recursos
-    sizea = size(a,1);
-    B = eye(sizea);
-    A=[a eye(sizea)];
-    ln = size(A,2);
-    C = [Z zeros(1,ln)];
-    BCol=[sizea:ln];
+    sizea = size(a,1)
+    B = eye(sizea)
+    A=[a eye(sizea)]
+    ln = size(A,2)
+    C = [Z zeros(1,ln - size(Z,2))]
+    BCol=[size(a,2)+1:ln]
     
     
     CBt = C(BCol(:));
