da3ed6bdd4be501418ed393b042d0aab9d8eb628
luis hernandez <nodnol4781@outlook.com>
2019-09-20 16:54:35 -0500

    Pruebas iteraciones

--- operacion.m
+++ operacion.m
@@ -11,26 +11,34 @@ function operacion()
     C = [Z zeros(1,ln - size(Z,2))];
     BCol=[size(a,2)+1:ln];
     
-    %iteracion 
-    CBt = C(BCol(:));
-    B1 = inv(B);
-    CBtB1 = CBt * B1;
-    for i=1:ln
-        Zi_Ci(i) = CBtB1 * A(:,i) - C(i);
+    terminar = false
+    
+    while ~terminar
+        %iteracion 
+        CBt = C(BCol(:));
+        B1 = inv(B);
+        CBtB1 = CBt * B1;
+        for i=1:ln
+            Zi_Ci(i) = CBtB1 * A(:,i) - C(i);
+        end
+        Zi_Ci
+        %busca los negativos(borrar cometario)
+        [Min_in Ind_in] = min(Zi_Ci);
+        if Min_in >= 0
+            terminar = true
+        end
+        %--------------------------------
+        fprintf('entra X%d\n',Ind_in);
+        B1b = B1 * b;
+        B1ai = B1 * A(:,Ind_in);
+        divX = DivX(B1b,B1ai);
+        [Min_out Ind_out] = min(divX);
+
+        fprintf('sale X%d\n',BCol(Ind_out));
+        BCol(Ind_out)= Ind_in;
+        B(:,Ind_out) = A(:,Ind_in)
     end
-    Zi_Ci
-    %busca los negativos(borrar cometario)
-    [Min_in Ind_in] = min(Zi_Ci);
-    %--------------------------------
-    fprintf('entra X%d\n',Ind_in);
-    B1b = B1 * b;
-    B1ai = B1 * A(:,Ind_in);
-    divX = DivX(B1b,B1ai);
-    [Min_out Ind_out] = min(divX)
-        
-    fprintf('sale X%d\n',BCol(Ind_out));
-    BCol(Ind_out)= Ind_in;
-    B(:,Ind_out) = A(:,Ind_in)
+    
     
     
     %Solucion de Z
