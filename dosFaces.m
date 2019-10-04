b414985cf9f59592a2eb1f152963dc94462a88d4
luis hernandez <nodnol4781@outlook.com>
2019-10-03 21:19:15 -0500

    resta renglones yi del Renglon objetivo

--- dosFaces.m
+++ dosFaces.m
@@ -1,6 +1,15 @@
 function dosFaces()
-tableau=input('tableau [maxz;restricciones <=> valores]= ');
-var_in_izq = input('ubicacion yis lado izquierdo= ')
+tableau=input('tableau [maxz;restricciones <=> valores]\n= ');
+var_in_izq = input('ubicacion yis lado izquierdo 0 si no yi \n= ');
 % primera fase
-
+RO = tableau(1,:)
+RO = RO - sumatoria(var_in_izq,tableau)
+end
+function suma = sumatoria(variz,tabl)
+    suma = zeros(1,size(tabl,2))
+    for i=1:size(variz,2)
+        if(variz(i) == 1)
+            suma = suma + tabl(i+1,:);
+        end
+    end
 end
