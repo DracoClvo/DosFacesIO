d7a8e1ecc149e27d0ee08ef650105a35836a700d
luis hernandez <nodnol4781@outlook.com>
2019-10-03 22:33:34 -0500

    primera fase terminada (a menos que las pruebas demuestren lo contrario)

--- dosFaces.m
+++ dosFaces.m
@@ -31,7 +31,7 @@ end
 function tab=ceros(tableau)
     for i=1:size(tableau,1)
         for j=1:size(tableau,2)
-            if tableau(i,j) < 10e-10
+            if abs(tableau(i,j)) < 10e-10
                tab(i,j) = round(tableau(i,j));
             else
                 tab(i,j) = tableau(i,j);
